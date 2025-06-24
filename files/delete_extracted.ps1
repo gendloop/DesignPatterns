a2dde741c09f542e486f4cdd52799720c0af10eb
gendloop <gendloop.cn@gmail.com>
2025-06-24 16:48:05 +0800

    chore: update

--- files/delete_extracted.ps1
+++
@@ -1,2 +0,0 @@
-Get-ChildItem -Path . -Directory | Remove-Item -Recurse -Force
-Get-ChildItem -Path . -Filter "*.props" | Remove-Item -Recurse -Force
--- files/package.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set name=%~n0
-powershell.exe -ExecutionPolicy RemoteSigned .\%name%.ps1
-choice -C YN -D Y -N -T 1
