8f9fcb86b99abfc1265ccf66a37dca22c8a4f43c
gendloop <gendloop.cn@gmail.com>
2025-07-08 16:59:35 +0800

    chore: update

---
+++ src/creational-patterns/builder.cpp
@@ -0,0 +1,67 @@
+﻿/**
+ * @file	builder.cpp
+ * @brief	建造者模式
+ * @author  gendloop
+ */
+
+// local
+#include "builder.h"
+
+MealItem::MealItem(const std::string& name, double price)
+    : name_(name), price_(price)
+{
+}
+
+std::string MealItem::getName() const
+{
+    return name_;
+}
+
+void MealItem::setName(const std::string& name)
+{
+    name_ = name;
+}
+
+double MealItem::getPrice() const
+{
+    return price_;
+}
+
+Hamburger::Hamburger()
+    : MealItem("Hamburger", 8.)
+{
+}
+
+void MealItem::setPrice(double price)
+{
+    price_ = price;
+}
+
+void Meal::addItem(const MealItemPtr& item)
+{
+    items_.push_back(item);
+}
+
+double Meal::getPrice() const
+{
+    double price = 0.0;
+    for(auto item : items_) {
+        price += item->getPrice();
+    }
+    return price;
+}
+
+void Meal::showItems() const
+{
+    for(auto item : items_) {
+        std::cout
+            << "MealItem: " << item->getName()
+            << ", Price: " << item->getPrice()
+            << std::endl;
+    }
+}
+
+Soda::Soda()
+    : MealItem("Soda", 4.)
+{
+}
---
+++ src/creational-patterns/builder.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+/**
+ * @file	builder.h
+ * @brief	建造者模式
+ * @author  gendloop
+ */
+
+// c++
+#include <vector>
+#include <string>
+#include <iostream>
+
+class MealItem {
+private:
+    std::string name_;
+    double price_;
+
+public:
+    MealItem(const std::string& name, double price);
+      
+    std::string getName() const;
+    void setName(const std::string& name);
+
+    double getPrice() const;
+    void setPrice(double price);
+};
+using MealItemPtr = std::shared_ptr<MealItem>;
+
+class Meal {
+private:
+    std::vector<MealItemPtr> items_;
+
+public:
+    void addItem(const MealItemPtr& item);
+    double getPrice() const;
+    void showItems() const;
+};
+
+class Soda : public MealItem {
+public:
+    Soda();
+};
+
+class Hamburger : public MealItem {
+public:
+    Hamburger();
+};
