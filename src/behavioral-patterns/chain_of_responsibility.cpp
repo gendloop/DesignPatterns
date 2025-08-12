ac885338065013f4e583287161e3b3acf83bb9d7
gendloop <gendloop.cn@gmail.com>
2025-08-12 17:00:44 +0800

    chore: upate

---
+++ src/behavioral-patterns/chain_of_responsibility.cpp
@@ -0,0 +1,13 @@
+﻿/**
+ * @file	chain_of_responsibility.cpp
+ * @brief	责任链模式
+ * @note    - PassNotes
+ * @author  gendloop
+ */
+
+// local
+#include "chain_of_responsibility.h"
+
+Request::Request(const std::string& content)
+{
+}
---
+++ src/behavioral-patterns/chain_of_responsibility.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+/**
+ * @file	chain_of_responsibility.h
+ * @brief	责任链模式
+ * @note    - PassNotes
+ * @author  gendloop
+ */
+
+// c++
+#include <iostream>
+#include <string>
+
+class Request {
+public:
+    Request(const std::string& content);
+
+    std::string getContent() const;
+
+private:
+    std::string content_;
+};
+
+class PassNotes {
+
+}
