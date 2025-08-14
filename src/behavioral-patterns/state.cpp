229ffaebe2db80713f3ed50fb80b0190f98dae51
gendloop <gendloop.cn@gmail.com>
2025-08-14 17:02:43 +0800

    chore: update

---
+++ src/behavioral-patterns/state.cpp
@@ -0,0 +1,6 @@
+﻿/**
+ * @file	state.cpp
+ * @brief	状态模式
+ * @note    - LightSwitch
+ * @author  gendloop
+ */
---
+++ src/behavioral-patterns/state.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+/**
+ * @file	state.h
+ * @brief	状态模式
+ * @note    - LightSwitch
+ * @author  gendloop
+ */
+
+// c++
+#include <iostream>
+
+enum LIGHT_STATE {
+    ON,
+    OFF
+};
+
+class LightState {
+public:
+    ~LightState() = default;
+    virtual void trigger() = 0;
+};
