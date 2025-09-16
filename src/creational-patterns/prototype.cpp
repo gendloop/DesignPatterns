fc2bf7c70b0a2215cbbf139b551456ec846a7080
gendloop <gendloop@163.com>
2025-09-16 21:28:01 +0800

    chore: update

---
+++ src/creational-patterns/prototype.cpp
@@ -0,0 +1,7 @@
+﻿/**
+ * @file	prototype.cpp
+ * @brief	原型模式
+ * @note    避免复杂初始化
+ * @note    - OneMore
+ * @author  gendloop
+ */
---
+++ src/creational-patterns/prototype.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+/**
+ * @file	prototype.h
+ * @brief	原型模式
+ * @note    避免复杂初始化
+ * @note    - OneMore
+ * @author  gendloop
+ */
+
+// c++
+#include <memory>
+#include <string>
+
+class Fruit {
+public:
+    virtual ~Fruit() = default;
+    virtual void clone() = 0;
+};
+
+class Apple : public Fruit {
+private:
+    std::string name_;
+    std::string color_;
+    double size_;
+
+public:
+    // undone
+};
+
+class Banana : public Fruit {
+private:
+    std::string name_;
+    std::string production_date_;
+    double length_;
+
+public:
+    // undone
+};
+
+class OneMore {
+public:
+    std::shared_ptr<Fruit> giveMeAnotherOne();
+};
